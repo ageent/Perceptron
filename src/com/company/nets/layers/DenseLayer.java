87a15758a4df768475fcf74409255ec06a2b1290
Eugene Chernov <ageent.10001@gmail.com>
2022-01-05 12:17:21 +0300

    Change parallelAffect().

--- src/com/company/nets/layers/DenseLayer.java
+++ src/com/company/nets/layers/DenseLayer.java
@@ -5,11 +5,9 @@ import com.company.nets.layers.neurons.ActivationFunc;
 import com.company.nets.layers.neurons.CombinationFunc;
 import com.company.nets.layers.neurons.Neuron;
 
-import java.util.Objects;
-import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class DenseLayer extends AbstractLayer {
@@ -34,26 +32,35 @@ public class DenseLayer extends AbstractLayer {
         return out;
     }
 
+    /**
+     * inputs.length == this.size
+     */
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public double[] parallelAffect(double... inputs) {
+        assert inputs.length == this.size() : "Different length of arrays!";
+
         int layerSize = this.size();
         double[] out = new double[layerSize];
+        AbstractNeuron[] layer = this.toArray(new AbstractNeuron[0]);
         AtomicInteger i = new AtomicInteger(0);
-        BlockingQueue<AbstractNeuron> queue = new LinkedBlockingQueue<>(layerSize);
-        ExecutorService executorService = Executors.newFixedThreadPool(layerSize);
+        ExecutorService pool = Executors.newFixedThreadPool(layerSize);
 
         for (AbstractNeuron n : this) {
-            while (!queue.offer(n)) ;
-            try {
-                executorService.submit(() -> {
-                    out[i.get()] = Objects.requireNonNull(queue.poll()).affect(inputs);
-                    i.getAndIncrement();
-                });
-            } catch (NullPointerException ignored) {
-            }
+            pool.submit(() -> {
+                int j = i.getAndIncrement();
+                out[j] = layer[j].affect(inputs);
+            });
         }
-        executorService.shutdown();
+        pool.shutdown();
+        try {
+            while (!pool.awaitTermination(1, TimeUnit.SECONDS)) ;
+        } catch (InterruptedException ie) {
+            // Cancel if current thread also interrupted
+            pool.shutdownNow();
+            // Preserve interrupt status
+            Thread.currentThread().interrupt();
+        }
 
         return out;
     }
